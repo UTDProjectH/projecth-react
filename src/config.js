5d2ded7a2fcb5b3ece5a3416ed6a38517d4ef172
Aniket Alur <aniket.alur13111997@gmail.com>
2021-09-12 12:47:52 -0500

    Added Login

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfkBsk-wraKbR5uGy1jd9oEbf9ZCiONLU",
+    authDomain: "projecth-589ec.firebaseapp.com",
+    databaseURL: "https://projecth-589ec-default-rtdb.firebaseio.com",
+    projectId: "projecth-589ec",
+    storageBucket: "projecth-589ec.appspot.com",
+    messagingSenderId: "78549388378",
+    appId: "1:78549388378:web:9eebaa848aaafba390dc63",
+    measurementId: "G-PNSM1FNRLX"
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app'
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const firebaseConfig = {
+    apiKey: "AIzaSyCfkBsk-wraKbR5uGy1jd9oEbf9ZCiONLU",
+    authDomain: "projecth-589ec.firebaseapp.com",
+    databaseURL: "https://projecth-589ec-default-rtdb.firebaseio.com",
+    projectId: "projecth-589ec",
+    storageBucket: "projecth-589ec.appspot.com",
+    messagingSenderId: "78549388378",
+    appId: "1:78549388378:web:9eebaa848aaafba390dc63",
+    measurementId: "G-PNSM1FNRLX"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+document.querySelector('#login-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var userEmail = document.getElementById("login_email_field").value;
+    var userPass = document.getElementById("login_password_field").value;
+
+    signInWithEmailAndPassword(auth, userEmail, userPass).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+    });
+})
+
+document.querySelector('#signup-button').addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    var userEmail = document.getElementById("signup_email_field").value;
+    var userPass = document.getElementById("signup_password_field").value;
+
+    createUserWithEmailAndPassword(auth, userEmail, userPass).catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+    });
+})
+
+document.querySelector('#logout_button').addEventListener('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    signOut(auth);
+
+    hideAll();
+    document.getElementById("home_div").style.display = 'block';
+
+})
+
+document.querySelector('#login-view-button').addEventListener('click', function (e) {
+    hideAll();
+    document.getElementById("login_div").style.display = 'block';
+})
+
+document.querySelector('#signup-view-button').addEventListener('click', function (e) {
+    hideAll();
+    document.getElementById("signup_div").style.display = 'block';
+})
+
+onAuthStateChanged(auth, (user) => {
+    hideAll();
+    if(user != null) {
+        document.getElementById("user_div").style.display = 'block';
+    } else {
+        document.getElementById("home_div").style.display = 'block';
+    }
+    console.log(user);
+})
+
+
+function hideAll() {
+    var elems = document.getElementsByClassName('main-div')
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        elem.style.display = 'none';
+    }
+
+    var elems = document.getElementsByClassName('loggedin-div')
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        elem.style.display = 'none';
+    }
+}
+
